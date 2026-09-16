a895f7bf89bb4801157d4e9fed75af70b53a2910
Lakshay Trehan <lakshay.trehan@inomic-solutions.com>
2026-09-16 23:04:55 +0200

    readme pdate

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { requireUser } from '../middlewares/requireUser';
 import { requireRole } from '../middlewares/requireRole';
-import { getSessions, revokeSession, getAllUsersAdmin } from '../controllers/user.controller';
+import { getSessions, revokeSession, getAllUsersAdmin, systemPing } from '../controllers/user.controller';
 
 const router = Router();
 
@@ -9,6 +9,7 @@ router.use(requireUser);
 
 router.get('/sessions', getSessions);
 router.delete('/sessions/:sessionId', revokeSession);
+router.get('/ping', systemPing);
 
 // Admin only route
 router.get('/admin/users', requireRole(['admin']), getAllUsersAdmin);
